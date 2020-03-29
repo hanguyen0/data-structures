e2706d9110a137b896f8a1e859de9a0c7eeb1ee2
uxsasukexnaruto <nguyha16@gmail.com>
2020-03-29 14:36:09 -0700

    finish set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function (item) {
+  this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function (item) {
+  if (this._storage[item]) {
+    return true;
+  }
+  return false;
 };
 
-setPrototype.remove = function(item) {
+setPrototype.remove = function (item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add method O(1)
+contains method O(1)
+remove method O(1)
  */
