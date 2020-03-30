d0ec4b1340f0dd2b518f863153b00ff49e6319cb
uxsasukexnaruto <nguyha16@gmail.com>
2020-03-29 21:00:34 -0700

    attempt binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,25 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function () {
+  let binaryST = {};
 
-};
+  binaryST.left = {};
+  binaryST.right = {};
+  //accepts a value and places it in the tree in the correct position.
+  binaryST.insert = function(value) {
+    if(value > binaryST.left){
+      binaryST.right = value;
+    }
+
+  }
+  //accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+  binaryST.contains = function(value) {
+
+  }
+  // accepts a callback and executes it on every value contained in the tree
+  binaryST.depthFirstLog = function(func) {
+
+  }
+  return binaryST;
+}
 
 
 /*
